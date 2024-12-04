eae0a00e39059ba51457acf3df58b0547af14f70
Yaser.ta <yasertarrab10@gmail.com>
2024-12-04 21:29:48 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let fruit = "apple";
+
+if (fruit === "apple" || fruit === "pear") {
+    console.log("This is a pome fruit.");
+} else if (fruit === "banana" || fruit === "mango") {
+    console.log("This is a tropical fruit.");
+} else {
+    console.log("Unknown fruit.");
+}
